10caa5094f4097423a3ab40037ae41168e185184
Jo-Anne <berne.nortier@dauphine.eu>
2023-01-12 23:45:25 +0100

    Changed and expand RCD

---
+++ Part 6/optilibrary.py
@@ -0,0 +1,19 @@
+def importData(p=0.2):
+    from sklearn import datasets
+    dataset = datasets.fetch_california_housing(as_frame = True)
+
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.model_selection import train_test_split
+    import numpy as np
+    np.random.seed(1)
+
+    dataset.frame_normalized = StandardScaler().fit_transform(dataset.frame)
+    # We drop Longitude as well since Latitude has enough information
+    X = dataset.frame_normalized[:,0:len(dataset.frame.columns) - 2]
+    y = dataset.frame_normalized[:,len(dataset.frame.columns) - 1]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = p, random_state = 9)
+    X_train = np.insert(X_train, 0, np.ones(X_train.shape[0]), axis=1)
+    X_test = np.insert(X_test, 0, np.ones(X_test.shape[0]), axis=1)
+
+    return X_train, y_train, X_test, y_test
