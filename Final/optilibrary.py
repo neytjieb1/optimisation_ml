6804ff0be8e529c59259ab0f921b1158cfcfab19
Jo-Anne <berne.nortier@dauphine.eu>
2023-01-14 09:31:34 +0100

    P3 so te sê klaar

--- Final/optilibrary.py
+++ Final/optilibrary.py
@@ -1,7 +1,6 @@
 def importData(p=0.2):
     from sklearn import datasets
     dataset = datasets.fetch_california_housing(as_frame = True)
-
     from sklearn.preprocessing import StandardScaler
     from sklearn.model_selection import train_test_split
     import numpy as np
@@ -9,10 +8,10 @@ def importData(p=0.2):
 
     dataset.frame_normalized = StandardScaler().fit_transform(dataset.frame)
     # We drop Longitude as well since Latitude has enough information
-    X = dataset.frame_normalized[:,0:len(dataset.frame.columns) - 2]
-    y = dataset.frame_normalized[:,len(dataset.frame.columns) - 1]
+    X = dataset.frame_normalized[:,0:-1]
+    y = dataset.frame_normalized[:,-1]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = p, random_state = 9)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = p, random_state = 16)
     X_train = np.insert(X_train, 0, np.ones(X_train.shape[0]), axis=1)
     X_test = np.insert(X_test, 0, np.ones(X_test.shape[0]), axis=1)
 
